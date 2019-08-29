6feb440b2c1c7e6136a5dab57b143ae03fc8f736
Sebastian Cardoso <sebastiancardoso92@gmail.com>
2019-08-29 17:51:12 -0300

    Capture click in cell

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -10,7 +10,7 @@ const Board = ({ board = [] }) => (
       {board.map((row, x) => (
         <tr key={x}>
           {board[x].map((cell, y) =>
-            <Cell key={`${x}${y}`} disk={cell.disk} allowed={cell.allowedCell} >{cell.id}</Cell>
+            <Cell key={`${x}${y}`} disk={cell.disk} allowed={cell.allowedCell} position={[x,y]} >{cell.id}</Cell>
           )}
         </tr>
       ))}
--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import '../assets/styles/Cell.css';
 
-const Cell = ({ children, disk, allowed }) => {
-  return (
-    <td className={allowed ? 'highlight' : 'cell'}>
-      {disk !== 'black' && disk !== 'white' ? children : null }
-      <div className={`${disk}` === 'black' ? 'black' : `${disk}` === 'white' ? 'white' : ''} />
-    </td>
-  )
-};
+class Cell extends Component {
+  render() {
+    const { disk, allowed, children } = this.props
+    return (
+      <td className={allowed ? 'highlight' : 'cell'} onClick={this.handleClick.bind(this)}>
+        {disk !== 'black' && disk !== 'white' ? children : null}
+        <div className={`${disk}` === 'black' ? 'black' : `${disk}` === 'white' ? 'white' : ''} />
+      </td>
+    )
+  }
+
+  handleClick() {
+    console.log('Has hecho click en:', this.props.position)
+  }
+
+}
 
 export default Cell;
