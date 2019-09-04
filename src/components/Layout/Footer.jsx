8506f64f04b960b35ef9788b17c28500e2884e0a
Sebastian Cardoso <sebastiancardoso92@gmail.com>
2019-09-03 21:02:56 -0300

    my mark

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import logo from '../../assets/logo.svg';
 import redux from '../../assets/redux.svg';
+import github from '../../assets/github.svg';
 
 import '../../assets/styles/Footer.css';
 
@@ -9,6 +10,10 @@ const Footer = () => (
   <footer className="footer">
     <img src={logo} className="App-logo-react" alt="logo" />
     <img src={redux} className="App-logo-redux" alt="redux" />
+    <a className="sc" href="http://sebastiancardoso.com">sc</a>
+    <a href="https://github.com/sebaveg/Reversi-game">
+      <img src={github} className="App-logo-github" alt="github" />
+    </a>
   </footer>
 );
 
